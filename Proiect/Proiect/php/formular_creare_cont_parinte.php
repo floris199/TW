6fa31aba6a48b3dda707831ced25403fbc0db06c
Andrei Liviu <andrei.liviuu@gmail.com>
2017-06-07 19:49:35 +0300

    Update formular_creare_cont_parinte.php

--- Proiect/Proiect/php/formular_creare_cont_parinte.php
+++ Proiect/Proiect/php/formular_creare_cont_parinte.php
@@ -3,7 +3,7 @@
                 <strong>Nume complet:</strong><br>
                 <?php
 					
-                    if (!isset($_COOKIE['empty_field1'])){
+                    if (!isset($_COOKIE['empty_field1']) and !isset($_COOKIE['caractere_interzise_in_nume'])){
                         echo '<input type="text" name="name" placeholder=" name"/><br>';
                     }else{
                         echo '<input type="text" name="name" placeholder=" name" id="wrong" /><br>';
@@ -28,7 +28,7 @@
 
                 echo '<strong>Parola:</strong><br>';            
                 
-                    if (!isset($_COOKIE['empty_field4']) and !isset($_COOKIE['pass_not_matching'])){
+                    if (!isset($_COOKIE['empty_field4']) and !isset($_COOKIE['pass_not_matching']) and !isset($_COOKIE['caractere_interzise_in_pass1']) and !isset($_COOKIE['caractere_putine_in_pass'])){
                         echo '<input type="password" name="pass" placeholder=" introduceti parola"/><br>';
                     }else{
 						echo '<input type="password" name="pass" placeholder=" introduceti parola" id="wrong"/><br>';
@@ -37,7 +37,7 @@
                 
                 echo '<strong>Reintroduceti parola:</strong><br>';
                 
-                    if (!isset($_COOKIE['empty_field5']) and !isset($_COOKIE['pass_not_matching'])){
+                    if (!isset($_COOKIE['empty_field5']) and !isset($_COOKIE['pass_not_matching']) and !isset($_COOKIE['caractere_interzise_in_pass2'])){
                         echo '<input type="password" placeholder=" reintroduceti parola" name="pass2"/><br>';
                     }else{
                         echo '<input type="password" placeholder=" reintroduceti parola" name="pass2" id="wrong"/><br>';
@@ -61,6 +61,17 @@
 					}
 					if(isset($_COOKIE['email_not_matching'])){
 						echo '<p style="color: red"> ***** Adresele de mail nu corespund<p><br>';
+					}
+					if(isset($_COOKIE['caractere_interzise_in_pass1']) or isset($_COOKIE['caractere_interzise_in_pass2']) or isset($_COOKIE['caractere_interzise_in_nume'])){
+						echo '<p style="color: red"> **** Ai folosit caractere interzise! <p><br>'; 
+						echo '<p style="color: red"> Caractere valide: <p>';
+						echo '<p style="color: red; font-size: 70%;"> Nume -> Litere mari si mici din alfabetul englez, spatiu si minus <p>';
+						echo '<p style="color: red; font-size: 70%;"> Parola -> Litere mari si mici din alfabetul englez si/sau cifre <p>';
+					}
+					if(isset($_COOKIE['caractere_putine_in_pass'])){
+						echo '<p style="color: red"> **** Numar mic de caractere <p><br>';
+						echo '<p style="color: red"> Numarul de caractere trebuie sa fie minim: <p>';
+						echo '<p style="color: red; font-size: 70%;"> 6 - pentru parola <p>';						
 					}
                 ?>
                 
